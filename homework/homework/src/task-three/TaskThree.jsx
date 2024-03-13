33f56f1b0ffad1272aa52a93ca20b8cb690ed559
Adam Khalguev <halgadam@gmail.com>
2024-03-13 20:41:46 +0300

    dz 3

--- homework/homework/src/task-three/TaskThree.jsx
+++ homework/homework/src/task-three/TaskThree.jsx
@@ -1,23 +1,29 @@
-import React, {useState} from 'react';
-import './TaskThree.css';
-
-// функция для получения данных с Mock API
-const fetchData = async (search) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?search=${search}`);
-    return await response.json();
-}
+import React from "react";
+import "./TaskThree.css";
+import useFetch from "./useFetch";
 
 export default function TaskThree() {
-    const [search, setSearch] = useState();
-    const [posts, setPosts] = useState([]);
-
-    return (
-        <div className="TaskThree">
-            <input type="text" onChange={(event) => setSearch(event.target.value)} placeholder="Search posts"/>
-            <h1>Posts</h1>
-            <ul>
-                {posts.map(item => <li key={item}>{item}</li>)}
-            </ul>
-        </div>
-    )
+  const [posts, loading, updateSearch] = useFetch();
+  return (
+    <div className="TaskThree">
+      <input
+        type="text"
+        onChange={(event) => updateSearch(event.target.value)}
+        placeholder="Search posts"
+      />
+      <h1>Posts</h1>
+      <ul>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+          posts.map((item) => (
+            <li key={item.id}>
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
